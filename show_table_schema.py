569022427e878f5862f63df2dd1a73160c9df6e4
Bisllly <cuongnguyen3900@gmail.com>
2024-01-06 18:22:31 +0700

    modified show_table_schema.py

--- show_table_schema.py
+++ show_table_schema.py
@@ -2,28 +2,32 @@ import os
 from dotenv import load_dotenv
 import subprocess
 
-load_dotenv()
+def main():
+    load_dotenv()
 
 
-database = os.getenv("DWH_DATABASE")
-user = os.getenv("DWH_USER")
-password = os.getenv("DWH_PWD")
-db_host = os.getenv("DWH_HOST")
+    database = os.getenv("DWH_DATABASE")
+    user = os.getenv("DWH_USER")
+    password = os.getenv("DWH_PWD")
+    db_host = os.getenv("DWH_HOST")
 
 
-ps_script = f"""
-$database = '{database}'
-$user = '{user}'
-$password = '{password}'
-$db_host = '{db_host}'
+    ps_script = f"""
+    $database = '{database}'
+    $user = '{user}'
+    $password = '{password}'
+    $db_host = '{db_host}'
 
-$tables = psql -h $db_host -U $user -d $database -t -c "SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema';"
+    $tables = psql -h $db_host -U $user -d $database -t -c "SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema';"
 
-foreach ($table in $tables) {{
-    Write-Host "Table: $table"
-    psql -h $db_host -U $user -d $database -c "SELECT * FROM $table LIMIT 5;"
-    Write-Host ""
-}}
-"""
+    foreach ($table in $tables) {{
+        Write-Host "Table: $table"
+        psql -h $db_host -U $user -d $database -c "SELECT * FROM $table LIMIT 5;"
+        Write-Host ""
+    }}
+    """
 
-subprocess.run(["powershell", "-Command", ps_script], check=True)
+    subprocess.run(["powershell", "-Command", ps_script], check=True)
+
+if __name__ == "__main__":
+  main()
